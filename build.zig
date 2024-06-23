a0233219e339b0cb4badb3979fdaefa4fc9012a5
Nico Elbers <nico.b.elbers@gmail.com>
2024-06-23 21:58:55 +0200

    Refactored build.zig

--- build.zig
+++ build.zig
@@ -1,84 +1,96 @@
 const std = @import("std");
 
+const Build = std.Build;
+const Step = Build.Step;
+const Module = Build.Module;
+const LazyPath = Build.LazyPath;
+const TestOptions = Build.TestOptions;
+
+const NamedModule = struct {
+    name: []const u8,
+    module: *Module,
+};
+
+fn makeModule(b: *Build, name: []const u8, options: Module.CreateOptions) NamedModule {
+    const mod = b.addModule(name, options);
+    return .{
+        .name = name,
+        .module = mod,
+    };
+}
+
+fn runnerPath(b: *Build) LazyPath {
+    return b.path("src/ztest-runner/runner.zig");
+}
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const ztest_runner_mod = b.addModule("ztest_runner", .{
-        .root_source_file = b.path("src/ztest-runner/runner.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const ztest_mod = b.addModule("ztest", .{
+    const ztest_mod = makeModule(b, "ztest", .{
         .root_source_file = b.path("src/ztest/ztest.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const ztest_runner_unit_tests = b.addTest(.{
-        .name = "ztest runner unit tests",
-        .root_source_file = b.path("src/ztest-runner/runner.zig"),
-        .target = target,
-        .optimize = optimize,
-        .test_runner = ztest_runner_mod.root_source_file,
-    });
-    const run_ztest_runner_unit_tests = b.addRunArtifact(ztest_runner_unit_tests);
-
-    const ztest_unit_tests = b.addTest(.{
-        .name = "ztest main unit tests",
-        .root_source_file = b.path("src/ztest/ztest.zig"),
-        .target = target,
-        .optimize = optimize,
-        .test_runner = ztest_runner_mod.root_source_file,
-    });
-    const run_ztest_unit_tests = b.addRunArtifact(ztest_unit_tests);
-
-    const unit_with_runner_tests = b.addTest(.{
-        .name = "Unit tests under ztest runner",
-        .root_source_file = b.path("tests/ztest/tests.zig"),
-        .target = target,
-        .optimize = optimize,
-        .test_runner = ztest_runner_mod.root_source_file,
-    });
-    unit_with_runner_tests.root_module.addImport("ztest", ztest_mod);
-    const run_unit_with_runner = b.addRunArtifact(unit_with_runner_tests);
-
-    const unit_without_runner_tests = b.addTest(.{
-        .name = "Unit tests under default runner",
-        .root_source_file = b.path("tests/ztest/tests.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    unit_without_runner_tests.root_module.addImport("ztest", ztest_mod);
-    const run_unit_without_runner = b.addRunArtifact(unit_without_runner_tests);
-
-    const readme_with_runner = b.addTest(.{
-        .name = "README code with runner",
-        .root_source_file = b.path("tests/readme/tests.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    readme_with_runner.root_module.addImport("ztest", ztest_mod);
-    const run_readme_with_runner = b.addRunArtifact(readme_with_runner);
-
-    const readme_without_runner = b.addTest(.{
-        .name = "README code without runner",
-        .root_source_file = b.path("tests/readme/tests.zig"),
-        .target = target,
-        .optimize = optimize,
-        .test_runner = ztest_runner_mod.root_source_file,
-    });
-    readme_without_runner.root_module.addImport("ztest", ztest_mod);
-    const run_readme_without_runner = b.addRunArtifact(readme_without_runner);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_ztest_runner_unit_tests.step);
-    test_step.dependOn(&run_ztest_unit_tests.step);
 
-    test_step.dependOn(&run_unit_with_runner.step);
-    test_step.dependOn(&run_unit_without_runner.step);
+    addMultiTest(b, test_step, &.{}, .{
+        .name = "ztest runner core unit tests",
+        .root_source_file = b.path("src/ztest-runner/runner.zig"),
+        .target = target,
+        .optimize = optimize,
+        .test_runner = runnerPath(b),
+    });
 
-    test_step.dependOn(&run_readme_with_runner.step);
-    test_step.dependOn(&run_readme_without_runner.step);
+    addMultiTest(b, test_step, &.{}, .{
+        .name = "ztest core unit tests",
+        .root_source_file = b.path("src/ztest/ztest.zig"),
+        .target = target,
+        .optimize = optimize,
+        .test_runner = runnerPath(b),
+    });
+
+    addMultiTest(b, test_step, &.{ztest_mod}, .{
+        .name = "Ztest unit tests",
+        .root_source_file = b.path("tests/ztest/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+        .test_runner = runnerPath(b),
+    });
+
+    addMultiTest(b, test_step, &.{ztest_mod}, .{
+        .name = "README code",
+        .root_source_file = b.path("tests/readme/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+}
+
+fn addMultiTest(b: *Build, test_step: *Step, deps: []const NamedModule, options: TestOptions) void {
+    // ----------- With runner --------------------
+    const options_with_runner = blk: {
+        var opt: TestOptions = options;
+        opt.test_runner = runnerPath(b);
+        break :blk opt;
+    };
+    const with_runner = b.addTest(options_with_runner);
+    for (deps) |dep| {
+        with_runner.root_module.addImport(dep.name, dep.module);
+    }
+    const run_with_runner = b.addRunArtifact(with_runner);
+    test_step.dependOn(&run_with_runner.step);
+
+    // ------------- Without runner -------------
+    const options_without_runner = blk: {
+        var opt: TestOptions = options;
+        opt.test_runner = null;
+        break :blk opt;
+    };
+    const without_runner = b.addTest(options_without_runner);
+    for (deps) |dep| {
+        without_runner.root_module.addImport(dep.name, dep.module);
+    }
+    const run_without_runner = b.addRunArtifact(without_runner);
+    test_step.dependOn(&run_without_runner.step);
 }
