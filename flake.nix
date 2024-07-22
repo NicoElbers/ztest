b993ac07b30cbb859c05eec546de1aa5cdaa11e9
Nico Elbers <nico.b.elbers@gmail.com>
2024-07-22 02:22:18 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -3,19 +3,31 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.zig.url = "github:mitchellh/zig-overlay";
 
-  outputs = { nixpkgs, flake-utils, zig, ... }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.default = with pkgs; mkShell {
-        packages = [ 
-          bashInteractive 
-          zig.packages.${system}.master
-        ];
-      };
-    });
+  inputs.zig = {
+    url = "github:mitchellh/zig-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  inputs.zls = {
+    url = "github:zigtools/zls";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, flake-utils, zls, zig, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        zls_bound = zls.packages.${system}.zls;
+        zig_bound = zig.packages.${system}.master;
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            bashInteractive
+            zig_bound
+            zls_bound
+          ];
+        };
+      });
 }
