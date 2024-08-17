7b960dab956f6583826c85b3aa0394c54f5faace
Nico Elbers <nico.b.elbers@gmail.com>
2024-08-17 14:50:44 +0200

    move parameterized test utilities to better places
    util/parameterized_tests.zig was a bit of an odd file, the parameterized
    test functions are now moved to parameterizedtest core and the
    callAnyFunction utility is straight in utils core

--- src/ztest/util/core.zig
+++ src/ztest/util/core.zig
@@ -1,11 +1,20 @@
-pub const parameterized_test = @import("parameterized_tests.zig");
-pub const parameterizedWrap = parameterized_test.wrap;
-pub const runAnyFunction = parameterized_test.callAnyFunction;
-pub const runTest = parameterized_test.runTest;
-pub const callAnyFunction = parameterized_test.callAnyFunction;
-
 pub const runner_com = @import("runner_communication.zig");
 pub const isUsingZtestRunner = runner_com.isUsingZtestRunner();
 // FIXME: useless function
 pub const setUsingZtest = runner_com.setUsingZtest;
 pub const RunnerInfo = runner_com.RunnerInfo;
+
+pub fn callAnyFunction(comptime func: anytype, args: anytype) !void {
+    const info = @typeInfo(@TypeOf(func));
+    if (info != .Fn)
+        @compileError("callAnyFunction takes in a function and argument tuple");
+
+    const RetT = info.Fn.return_type.?;
+    switch (@typeInfo(RetT)) {
+        .ErrorUnion,
+        .ErrorSet,
+        => _ = try @call(.auto, func, args),
+
+        else => _ = @call(.auto, func, args),
+    }
+}
