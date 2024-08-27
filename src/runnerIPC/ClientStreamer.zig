dba1043ba62049bc8c52eee8888ef796def23ac8
Nico Elbers <nico.b.elbers@gmail.com>
2024-08-27 20:39:24 +0200

    cleanup IPC
    Change the IPC api a little bit, to make it much easier to instanciate a
    server/ client without a child process for testing.
    
    Also consolidate some duplicated code in streamerUtils, so that chaning
    things in the future will be easier

--- src/runnerIPC/ClientStreamer.zig
+++ src/runnerIPC/ClientStreamer.zig
@@ -2,24 +2,15 @@ stdin_content: std.ArrayList(u8),
 delim_checked_ptr: usize,
 poller: std.io.Poller(PollerEnum),
 
-pub const timeout_ns = time.ns_per_ms * 5;
-
 const PollerEnum = enum { stdin };
 
 pub const Error = error{
     StreamClosed,
 } || Allocator.Error || Errors(std.io.Poller(PollerEnum).pollTimeout);
 
-pub const ReadStatus = union(enum) {
-    StreamClosed: void,
-    TimedOut: void,
-    ReadLen: usize,
-    DelimeterEnd: usize,
-};
-
-pub fn init(alloc: Allocator) ClientStreamer {
+pub fn init(alloc: Allocator, stdin: File) ClientStreamer {
     const poller = std.io.poll(alloc, PollerEnum, .{
-        .stdin = std.io.getStdIn(),
+        .stdin = stdin,
     });
 
     return ClientStreamer{
@@ -66,61 +57,31 @@ pub fn read(self: *ClientStreamer) Error!ReadStatus {
 
 /// Polls until a delimeter is found, the last poll returned nothing or the stream has ended.
 /// If a delimeter is found, returns the index after the delimeter
-pub fn readUntilDelimeter(self: *ClientStreamer, comptime delimeter: []const u8) Error!ReadStatus {
-    comptime assert(delimeter.len > 0);
-
-    var total_len_read: usize = 0;
-    while (true) {
-        if (self.delim_checked_ptr >= self.stdin_content.items.len -| delimeter.len) {
-            switch (try self.read()) {
-                .ReadLen => |len| total_len_read += len,
-                .TimedOut => if (total_len_read == 0) {
-                    return .TimedOut;
-                } else {
-                    return .{ .ReadLen = total_len_read };
-                },
-                else => |status| return status,
-            }
-        }
-
-        const unseen_slice = self.stdin_content.items[self.delim_checked_ptr..];
-
-        print("Found unseen: {s}", .{unseen_slice});
-
-        if (unseen_slice.len < delimeter.len) continue;
-
-        inner: for (0..(unseen_slice.len - delimeter.len + 1)) |start_ptr| {
-            const check_slice = unseen_slice[start_ptr..(start_ptr + delimeter.len)];
-            assert(check_slice.len == delimeter.len);
-
-            // TODO: Be a little bit smarter, and move up delim_checked_ptr while
-            // looping
-            for (check_slice, delimeter) |check_item, delim_item| {
-                if (check_item != delim_item) continue :inner;
-            }
-
-            self.delim_checked_ptr += start_ptr + delimeter.len;
-            return .{ .DelimeterEnd = self.delim_checked_ptr };
-        }
-
-        self.delim_checked_ptr += unseen_slice.len - delimeter.len + 1;
-    }
-    unreachable;
+pub inline fn readUntilDelimeter(self: *ClientStreamer, comptime delimeter: []const u8) Error!ReadStatus {
+    return try streamerUtils.readUntilDelimeter(
+        self,
+        delimeter,
+        &self.stdin_content,
+        &self.delim_checked_ptr,
+    );
 }
 
 pub fn Errors(comptime Fn: anytype) type {
     return @typeInfo(@typeInfo(@TypeOf(Fn)).Fn.return_type.?).ErrorUnion.error_set;
 }
 
-pub fn print(comptime fmt: []const u8, args: anytype) void {
+fn print(comptime fmt: []const u8, args: anytype) void {
     std.debug.print(fmt ++ "\n", args);
 }
 
 const ClientStreamer = @This();
 
+const ReadStatus = streamerUtils.ReadStatus;
 const Allocator = std.mem.Allocator;
 const File = std.fs.File;
+const timeout_ns = streamerUtils.timeout_ns;
 
 const assert = std.debug.assert;
 const std = @import("std");
 const time = std.time;
+const streamerUtils = @import("streamerUtils.zig");
---
+++ src/runnerIPC/streamerUtils.zig
@@ -0,0 +1,69 @@
+pub const timeout_ns = time.ns_per_ms * 5;
+
+pub const ReadStatus = union(enum) {
+    StreamClosed: void,
+    TimedOut: void,
+    ReadLen: usize,
+    DelimeterEnd: usize,
+};
+
+/// Shared delim reader for both the client and the server.
+/// streamer must be a function called read, that updates the to_check array.
+/// checked_ptr is the location inside the to_check array which has already been
+/// checked for delimeters.
+pub fn readUntilDelimeter(
+    streamer: anytype,
+    comptime delimeter: []const u8,
+    to_check: *std.ArrayList(u8),
+    checked_ptr: *usize,
+) !ReadStatus {
+    comptime assert(delimeter.len > 0);
+
+    var total_len_read: usize = 0;
+    while (true) {
+        if (checked_ptr.* >= to_check.items.len -| delimeter.len) {
+            switch (try streamer.read()) {
+                .ReadLen => |len| total_len_read += len,
+                .TimedOut => if (total_len_read == 0) {
+                    return .TimedOut;
+                } else {
+                    return .{ .ReadLen = total_len_read };
+                },
+                else => |status| return status,
+            }
+        }
+
+        const unseen_slice = to_check.items[streamer.delim_checked_ptr..];
+
+        if (unseen_slice.len < delimeter.len) continue;
+
+        inner: for (0..(unseen_slice.len - delimeter.len + 1)) |start_ptr| {
+            const check_slice = unseen_slice[start_ptr..(start_ptr + delimeter.len)];
+            assert(check_slice.len == delimeter.len);
+
+            // TODO: Be a little bit smarter, and move up delim_checked_ptr while
+            // looping
+            for (check_slice, delimeter) |check_item, delim_item| {
+                if (check_item != delim_item) continue :inner;
+            }
+
+            checked_ptr.* += start_ptr + delimeter.len;
+            return .{ .DelimeterEnd = checked_ptr.* };
+        }
+
+        checked_ptr.* += unseen_slice.len - delimeter.len + 1;
+    }
+    unreachable;
+}
+
+pub fn Errors(comptime Fn: anytype) type {
+    return @typeInfo(@typeInfo(@TypeOf(Fn)).Fn.return_type.?).ErrorUnion.error_set;
+}
+
+const Allocator = std.mem.Allocator;
+const File = std.fs.File;
+const Poller = std.io.Poller;
+
+const assert = std.debug.assert;
+const std = @import("std");
+const time = std.time;
