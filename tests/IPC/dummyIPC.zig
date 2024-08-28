6a3902b21c2de31512842ece8d7e6abe4f908097
Nico Elbers <nico.b.elbers@gmail.com>
2024-08-28 02:35:39 +0200

    tests :D

---
+++ tests/IPC/dummyIPC.zig
@@ -0,0 +1,240 @@
+const gpa = ztest.allocator;
+
+const expect = ztest.expect;
+
+const File = std.fs.File;
+const Server = IPC.Server;
+const ServerStreamer = IPC.ServerStreamer;
+const Client = IPC.Client;
+const ClientStreamer = IPC.ClientStreamer;
+const Message = IPC.Message;
+
+test "server timeout" {
+    var setup = IPCSetup.setup();
+    defer setup.cleanup();
+
+    const server = &setup.server;
+
+    // Wait for one second
+    const res = waitForMessageTimeout(server, msg_timeout);
+
+    // try expect(res).isError(@errorCast(error.MessageTookTooLong));
+    // https://github.com/ziglang/zig/issues/21222
+    _ = res catch |err| switch (err) {
+        error.MessageTookTooLong => return,
+        else => return err,
+    };
+    return error.NotAnError;
+}
+
+test "client timeout" {
+    var setup = IPCSetup.setup();
+    defer setup.cleanup();
+
+    const client = &setup.client;
+
+    // Wait for one second
+    const res = waitForMessageTimeout(client, msg_timeout);
+
+    // try expect(res).isError(@errorCast(error.MessageTookTooLong));
+    // https://github.com/ziglang/zig/issues/21222
+    _ = res catch |err| switch (err) {
+        error.MessageTookTooLong => return,
+        else => return err,
+    };
+    return error.NotAnError;
+}
+
+test "server clean message" {
+    var setup = IPCSetup.setup();
+    defer setup.cleanup();
+
+    const server = &setup.server;
+    const client = &setup.client;
+
+    const server_message = "Hello from server";
+    try server.serveStringMessage(.rawString, server_message);
+
+    const message = try waitForMessageTimeout(client, msg_timeout);
+    defer gpa.free(message.bytes);
+
+    try expect(message).isEqualTo(Message{
+        .header = .{ .tag = .rawString, .bytes_len = server_message.len },
+        .bytes = server_message,
+    });
+}
+
+test "client clean message" {
+    var setup = IPCSetup.setup();
+    defer setup.cleanup();
+
+    const server = &setup.server;
+    const client = &setup.client;
+
+    const client_message = "Hello from client";
+    try client.serveStringMessage(.rawString, client_message);
+
+    const message = try waitForMessageTimeout(server, msg_timeout);
+    defer gpa.free(message.bytes);
+
+    try expect(message).isEqualTo(Message{
+        .header = .{ .tag = .rawString, .bytes_len = client_message.len },
+        .bytes = client_message,
+    });
+}
+
+test "active communication" {
+    var setup = IPCSetup.setup();
+    defer setup.cleanup();
+
+    const server = &setup.server;
+    const client = &setup.client;
+
+    const msg1_expected = "raw string";
+    try server.serveStringMessage(.rawString, msg1_expected);
+
+    const msg1 = try waitForMessageTimeout(client, msg_timeout);
+    defer gpa.free(msg1.bytes);
+
+    try expect(msg1).isEqualTo(Message{
+        .header = .{ .tag = .rawString, .bytes_len = msg1_expected.len },
+        .bytes = msg1_expected,
+    });
+
+    const msg2_expected = "pram skipped";
+    try client.serveStringMessage(.parameterizedSkipped, msg2_expected);
+
+    const msg2 = try waitForMessageTimeout(server, msg_timeout);
+    defer gpa.free(msg2.bytes);
+
+    try expect(msg2).isEqualTo(Message{
+        .header = .{ .tag = .parameterizedSkipped, .bytes_len = msg2_expected.len },
+        .bytes = msg2_expected,
+    });
+
+    try server.serveExit();
+
+    const msg3 = try waitForMessageTimeout(client, msg_timeout);
+    try expect(msg3).isEqualTo(Message{
+        .header = .{ .tag = .exit, .bytes_len = 0 },
+        .bytes = &.{},
+    });
+}
+
+pub const Pipe = struct {
+    read_file: File,
+    write_file: File,
+
+    pub fn createPipe() !Pipe {
+        if (os == .windows) unreachable;
+
+        const fds = try posix.pipe2(.{ .CLOEXEC = true });
+        return .{
+            .read_file = .{ .handle = fds[0] },
+            .write_file = .{ .handle = fds[1] },
+        };
+    }
+
+    pub fn destroy(self: Pipe) void {
+        if (os == .windows) unreachable;
+
+        if (self.read_file.handle != -1) std.posix.close(self.read_file.handle);
+        if (self.read_file.handle != self.write_file.handle) std.posix.close(self.write_file.handle);
+    }
+};
+
+pub fn makeDummyServer(child_stdin: File, child_stdout: File, child_stderr: File) Server {
+    return Server{
+        .process_streamer = ServerStreamer.init(
+            gpa,
+            child_stdout,
+            child_stderr,
+        ),
+        .out = child_stdin,
+    };
+}
+
+pub fn makeDummyClient(child_stdin: File, child_stdout: File) Client {
+    return Client{
+        .process_streamer = ClientStreamer.init(
+            gpa,
+            child_stdin,
+        ),
+        .out = child_stdout,
+    };
+}
+
+pub fn waitForMessageTimeout(node: anytype, timeout_ns: u64) !Message {
+    const Instant = std.time.Instant;
+    const timer = try Instant.now();
+    while ((try Instant.now()).since(timer) <= timeout_ns) {
+        switch (try node.receiveMessage(gpa)) {
+            .StreamClosed => return error.StreamClosed,
+            .TimedOut => continue,
+            .Message => |msg| return msg,
+        }
+    }
+    return error.MessageTookTooLong;
+}
+
+const msg_timeout = std.time.ns_per_ms * 100;
+
+pub const IPCSetup = struct {
+    stdin: Pipe,
+    stdout: Pipe,
+    stderr: Pipe,
+
+    server: Server,
+    client: Client,
+
+    pub fn setup() IPCSetup {
+        const stdin = Pipe.createPipe() catch @panic("setup panic");
+        errdefer stdin.destroy();
+
+        const stdout = Pipe.createPipe() catch @panic("setup panic");
+        errdefer stdout.destroy();
+
+        const stderr = Pipe.createPipe() catch @panic("setup panic");
+        errdefer stderr.destroy();
+
+        var server = makeDummyServer(
+            stdin.write_file,
+            stdout.read_file,
+            stderr.read_file,
+        );
+        errdefer server.deinit();
+
+        var client = makeDummyClient(
+            stdin.read_file,
+            stdout.write_file,
+        );
+        errdefer client.deinit();
+
+        return IPCSetup{
+            .stdin = stdin,
+            .stdout = stdout,
+            .stderr = stderr,
+
+            .server = server,
+            .client = client,
+        };
+    }
+
+    pub fn cleanup(self: *IPCSetup) void {
+        self.stdin.destroy();
+        self.stdout.destroy();
+        self.stderr.destroy();
+
+        self.server.deinit();
+        self.client.deinit();
+    }
+};
+
+const std = @import("std");
+const ztest = @import("ztest");
+const IPC = @import("IPC");
+const builtin = @import("builtin");
+
+const os = builtin.os.tag;
+const windows = std.os.windows;
+const posix = std.posix;
