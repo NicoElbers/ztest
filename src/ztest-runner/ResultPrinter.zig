ad5f518c252e2aeef2a8498ec69624bdc78b123e
Nico Elbers <nico.b.elbers@gmail.com>
2024-09-07 18:59:48 +0200

    ResultPrinter: Completely rework
    Internal representation of tests is completely overhauled to properly
    store the relation between normal tests and their parameterized tests.
    
    Notably, it is currently not possible to change anything but the most
    recent parameterized test for a given parent test. This is because it
    was a nightmare to figure out otherwise. I would need to create
    something along the lines of an ID for every parameterized test, since
    we can't assume that the arg_fmt is unique. I'll maybe have to revisit
    this in the future but for now this works for what I'm doing.

--- src/ztest-runner/ResultPrinter.zig
+++ src/ztest-runner/ResultPrinter.zig
@@ -1,100 +1,171 @@
+printer: Printer,
+alloc: Allocator,
+tests: []?TestInformation,
+ptests: std.ArrayList(ParameterizedInformation),
+printed_lines: u15,
+
+pub const Status = union(enum) { busy, passed, skipped, failed: anyerror };
+
+pub const TestInformation = struct {
+    status: Status,
+
+    /// Represents the index into the ptest array where the parameterized test
+    /// can be found
+    ptests: std.ArrayList(usize),
+
+    pub fn deinit(self: *TestInformation) void {
+        self.ptests.deinit();
+        self.* = undefined;
+    }
+};
+
+pub const ParameterizedInformation = struct {
+    args_fmt: []const u8,
+    parent_test_idx: usize,
+    status: Status,
+
+    pub fn deinit(self: *ParameterizedInformation, alloc: Allocator) void {
+        alloc.free(self.args_fmt);
+        self.* = undefined;
+    }
+};
+
+pub fn init(alloc: Allocator, test_amount: usize, output_file: File) !Self {
+    const printer = Printer.init(output_file, alloc);
+
+    // We can prealloc this because we get all the top level tests at comptime,
+    // however for testability reasons, I still want to allocate at runtime
+    const tests = try alloc.alloc(?TestInformation, test_amount);
+    @memset(tests, null);
+
+    return Self{
+        .printer = printer,
+        .tests = tests,
+        .ptests = std.ArrayList(ParameterizedInformation).init(alloc),
+        .alloc = alloc,
+        .printed_lines = 0,
+    };
+}
+
+pub fn deinit(self: *Self) void {
+    for (self.ptests.items) |*ptest| {
+        ptest.deinit(self.alloc);
+    }
+    self.ptests.deinit();
+
+    for (self.tests) |*tst| {
+        if (tst.*) |*t| {
+            t.deinit();
+        }
+    }
+
+    self.alloc.free(self.tests);
+    self.* = undefined;
+}
+
+pub fn initTest(self: *Self, test_idx: usize) void {
+    assert(test_idx < self.tests.len);
+    assert(self.tests[test_idx] == null);
+
+    self.tests[test_idx] = .{
+        .status = .busy,
+        .ptests = std.ArrayList(usize).init(self.alloc),
+    };
+}
+
+pub fn initParameterizedTest(
+    self: *Self,
+    parent_test_idx: usize,
+    args_fmt: []const u8,
+) !void {
+    assert(parent_test_idx < self.tests.len);
+    assert(self.tests[parent_test_idx] != null);
+
+    try self.tests[parent_test_idx].?.ptests.append(self.ptests.items.len);
+
+    try self.ptests.append(.{
+        .args_fmt = try self.alloc.dupe(u8, args_fmt),
+        .parent_test_idx = parent_test_idx,
+        .status = .busy,
+    });
+}
+
+pub fn updateTest(self: Self, test_idx: usize, status: Status) void {
+    assert(test_idx < self.tests.len);
+    assert(self.tests[test_idx] != null);
+
+    self.tests[test_idx].?.status = status;
+}
+
+pub fn updateLastPtest(self: *Self, parent_test_idx: usize, status: Status) void {
+    assert(parent_test_idx < self.tests.len);
+    assert(self.tests[parent_test_idx] != null);
+
+    const parent = self.tests[parent_test_idx].?;
+    const idx = parent.ptests.items[parent.ptests.items.len - 1];
+    self.ptests.items[idx].status = status;
+}
+
+/// Prints to the provided printer
+pub fn printResults(self: *Self, tests: []const std.builtin.TestFn) !void {
+    const printer = &self.printer;
+
+    try self.clearPrinted();
+
+    for (self.tests, 0..) |test_maybe, idx| {
+        const test_name = tests[idx].name;
+        try printer.writeAll(test_name);
+        try printer.writeAll(": ");
+
+        if (test_maybe) |test_info| {
+            switch (test_info.status) {
+                .busy => {
+                    try printer.setColor(.dim);
+                    try printer.writeAll("busy");
+                },
+                .passed => {
+                    try printer.setColor(.bright_green);
+                    try printer.writeAll("passed");
+                },
+                .skipped => {
+                    try printer.setColor(.bright_yellow);
+                    try printer.writeAll("skipped");
+                },
+                .failed => |err| {
+                    try printer.setColor(.red);
+                    try printer.writeAll("failed (");
+                    try printer.writeAll(@errorName(err));
+                    try printer.writeAll(")");
+                },
+            }
+        } else {
+            try printer.setColor(.dim);
+            try printer.writeAll("waiting");
+        }
+
+        try printer.setColor(.reset);
+        try self.newLine();
+    }
+}
+
+fn newLine(self: *Self) !void {
+    try self.printer.writeAll("\n");
+
+    // If we would overflow, just saturate
+    self.printed_lines +|= 1;
+}
+
+pub fn clearPrinted(self: *Self) !void {
+    try self.printer.moveUpLine(self.printed_lines);
+    try self.printer.clearBelow();
+    self.printed_lines = 0;
+}
+
 const std = @import("std");
+const builtin = @import("builtin");
 const assert = std.debug.assert;
 
 const Allocator = std.mem.Allocator;
 const File = std.fs.File;
 const Printer = @import("Printer.zig");
 const Self = @This();
-
-printer: Printer,
-results: std.StringArrayHashMap(TestInformation),
-
-pub const TestInformation = struct {
-    args_fmt: ?[]const u8,
-    status: Status,
-
-    pub const Status = union(enum) { busy, passed, skipped, failed: anyerror };
-};
-
-pub fn init(alloc: Allocator, output_file: File) Self {
-    const printer = Printer.init(output_file, alloc);
-
-    return Self{
-        .printer = printer,
-        .results = std.StringArrayHashMap(TestInformation).init(alloc),
-    };
-}
-
-pub fn deinit(self: *Self) void {
-    self.results.deinit();
-}
-
-pub fn initTest(self: *Self, name: []const u8, args_fmt: ?[]const u8) void {
-    const info: TestInformation = .{
-        .args_fmt = args_fmt,
-        .status = .busy,
-    };
-
-    self.results.putNoClobber(name, info) catch
-        @panic("Test initialization error");
-}
-
-pub fn updateTest(self: *Self, name: []const u8, status: TestInformation.Status) void {
-    const info = self.results.getPtr(name) orelse {
-        @panic("Cannot update test that hasn't been registered");
-    };
-
-    info.status = status;
-}
-
-/// Prints to the provided printer
-pub fn printResults(self: Self) !void {
-    var iter = self.results.iterator();
-
-    while (iter.next()) |entry| {
-        const name = entry.key_ptr.*;
-        const info = entry.value_ptr;
-
-        const status_tag = std.meta.activeTag(info.status);
-
-        // Don't print anything for passing parameterized tests
-        if (status_tag == .passed and info.args_fmt != null) continue;
-
-        const printer = self.printer;
-
-        try printer.writeAll(name);
-        try printer.writeAll(": ");
-
-        switch (info.status) {
-            .passed => {
-                try printer.setColor(.bright_green);
-                try printer.writeAll("passed");
-                try printer.setColor(.reset);
-            },
-            .skipped => {
-                try printer.setColor(.bright_yellow);
-                try printer.writeAll("skipped");
-                try printer.setColor(.reset);
-            },
-            .busy => {
-                try printer.setColor(.dim);
-                try printer.writeAll("busy");
-                try printer.setColor(.reset);
-            },
-            .failed => |err| {
-                if (info.args_fmt) |args| {
-                    try printer.setColor(.dim);
-                    try printer.writeAll("(");
-                    try printer.writeAll(args);
-                    try printer.writeAll(") ");
-                }
-
-                try printer.setColor(.bright_red);
-                try printer.writeAll("failed ");
-                try printer.writeAll(@errorName(err));
-                try printer.setColor(.reset);
-            },
-        }
-        try printer.writeAll("\n");
-    }
-}
