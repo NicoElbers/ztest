c30721e6862d1f44cff4706b26da0ec0f514a5a3
Nico Elbers <nico.b.elbers@gmail.com>
2024-09-11 16:51:36 +0200

    Only run 1 test at a time
    To properly seperate the stdout and stderr from the test, make sure to
    only run 1 test at a time

--- src/runnerIPC/Message.zig
+++ src/runnerIPC/Message.zig
@@ -12,9 +12,9 @@ pub const Tag = enum(u32) {
     /// Has no body, can only be sent by server
     exit,
 
-    /// Body consists of 2 usizes, the first the start index in builtin.test_functions
-    /// and the second the end index (exclusive). Can only be sent by the server
-    runTests,
+    /// Body consists of 1 usize, representing index in builtin.test_functions
+    /// Can only be sent by the server
+    runTest,
 
     /// Body consists of 1 usize, indicating the index of the test being run.
     /// Can only be sent by the client.
