a0481552a451f41280c675c3c7a27414c8f7f78e
Nico Elbers <nico.b.elbers@gmail.com>
2024-09-17 01:40:02 +0200

    terminal test

--- test.zig
+++ test.zig
@@ -1,110 +1,143 @@
 const std = @import("std");
-const IPC = @import("src/runnerIPC/root.zig");
+const posix = std.posix;
+const assert = std.debug.assert;
 
-const Server = IPC.Server;
-const Client = IPC.Client;
-const Child = std.process.Child;
-const Allocator = std.mem.Allocator;
+const termios = posix.termios;
+const File = std.fs.File;
 
-fn print(comptime msg: []const u8) void {
-    printf(msg, .{});
-}
-
-fn printf(comptime fmt: []const u8, args: anytype) void {
-    std.debug.print(fmt ++ "\n", args);
-}
+const csi = [_]u8{ 0x1b, '[' };
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const alloc = gpa.allocator();
+    const stdout = std.io.getStdOut();
 
-    const args = try std.process.argsAlloc(alloc);
-    defer std.process.argsFree(alloc, args);
-    if (args.len == 1) {
-        return parentFn(args[0], alloc);
-    } else {
-        return childFn(alloc);
+    const count = 1000;
+
+    var total: u64 = 0;
+    for (0..count) |_| {
+        const start = try std.time.Instant.now();
+        _ = try getTerminalWidth(stdout) orelse 80;
+        const end = try std.time.Instant.now();
+        total += end.since(start);
+    }
+
+    const width = try getTerminalWidth(stdout) orelse 80;
+
+    std.debug.print("Getting terminal width took {d}ms on average\n", .{@divTrunc(total, std.time.ns_per_ms * count)});
+
+    for (0..width) |_| {
+        std.debug.print("-", .{});
+    }
+    std.debug.print("\n", .{});
+}
+
+pub fn getTerminalWidth(file: File) !?u16 {
+    // Untested windows implementation
+    // https://stackoverflow.com/a/23370070
+    if (@import("builtin").os.tag == .windows) {
+        const windows = std.os.windows;
+        const kernel32 = windows.kernel32;
+
+        const csbi: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
+        kernel32.GetConsoleScreenBufferInfo(file.handle, &csbi);
+
+        const width: i16 = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        assert(width >= 0);
+        return @intCast(width);
+    }
+
+    // Use the existance of the termios struct as a heuristic to whether we can
+    // query the terminal on this system
+    if (!@hasDecl(posix, "termios"))
+        return null;
+
+    var buf: [64]u8 = undefined;
+
+    // Format returned by `CSI 18 t` is `CSI 8 ; height ; width t`
+    // https://terminalguide.namepad.de/seq/csi_st-18/
+    const res = try terminalQuery("18t", file.handle, &buf);
+    var spliterator = std.mem.splitScalar(u8, res, ';');
+
+    assert(std.mem.eql(u8, spliterator.first(), [_]u8{0x1b} ++ "[8"));
+
+    // Ignore the height
+    assert(spliterator.next() != null);
+
+    const width_buf_full = spliterator.next().?;
+    const end_idx = std.mem.indexOfScalar(u8, width_buf_full, 't').?;
+    const width_buf = width_buf_full[0..end_idx];
+    return try std.fmt.parseInt(u16, width_buf, 10);
+}
+
+/// Your message will be prepended with CSI
+fn terminalQuery(comptime msg: []const u8, fd: std.posix.fd_t, buf: []u8) ![]const u8 {
+    const bak = try rawModeOn(fd);
+    defer termiosReset(fd, bak) catch
+        @panic("Terminal left in raw mode, please reset it");
+
+    var written: usize = 0;
+    while (written < csi.len + msg.len) {
+        written += try std.posix.write(fd, csi ++ msg);
+    }
+
+    const res = try std.posix.read(fd, buf);
+    return buf[0..res];
+}
+
+/// Your message will be prepended with CSI
+fn terminalMessage(comptime msg: []const u8, fd: std.posix.fd_t) !void {
+    const bak = try rawModeOn(fd);
+    defer termiosReset(fd, bak) catch
+        @panic("Terminal left in raw mode, please reset it");
+
+    var written: usize = 0;
+    while (written < csi.len + msg.len) {
+        written += try std.posix.write(fd, csi ++ msg);
     }
 }
 
-pub fn childFn(gpa: Allocator) !void {
-    var client = Client.init(gpa);
-    defer client.deinit();
-    defer print("Client shutting down");
+fn rawModeOn(fd: std.posix.fd_t) !std.posix.termios {
+    if (!@hasDecl(std.posix, "termios"))
+        @compileError("Cannot enable raw mode on this architechture");
 
-    try client.serveStringMessage(.rawString, "Hello from client!");
+    var term = try std.posix.tcgetattr(fd);
+    const copy = term;
 
-    while (true) {
-        const message = switch (try client.receiveMessage(gpa)) {
-            .Message => |msg| msg,
-            .TimedOut => {
-                continue;
-            },
-            .StreamClosed => {
-                print("Stream closed");
-                return;
-            },
-        };
-        defer gpa.free(message.bytes);
+    // zig fmt: off
+    // Raw mode from man termios(3)
+    term.iflag.BRKINT = false;
+    term.iflag.ICRNL  = false;
+    term.iflag.IGNBRK = false;
+    term.iflag.IGNCR  = false;
+    term.iflag.INLCR  = false;
+    term.iflag.ISTRIP = false;
+    term.iflag.IXON   = false;
+    term.iflag.PARMRK = false;
 
-        switch (message.header.tag) {
-            .rawString => printf("client received: {s}", .{message.bytes}),
-            .exit => {
-                print("Client received exit");
-                return;
-            },
-            else => unreachable,
-        }
-    }
+    term.oflag.OPOST  = false;
+
+    term.lflag.ECHO   = false;
+    term.lflag.ECHONL = false;
+    term.lflag.ICANON = false;
+    term.lflag.IEXTEN = false;
+    term.lflag.ISIG   = false;
+
+    term.cflag.CSIZE  = .CS8;
+    term.cflag.PARENB = false;
+    // zig fmt: on
+
+    try std.posix.tcsetattr(
+        fd,
+        std.posix.TCSA.FLUSH,
+        term,
+    );
+
+    return copy;
 }
 
-pub fn parentFn(arg0: [:0]const u8, gpa: Allocator) !void {
-    var child = Child.init(&.{ arg0, "asdf" }, gpa);
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Pipe;
-    child.stdin_behavior = .Pipe;
-
-    try child.spawn();
-    // errdefer _ = child.kill() catch @panic(":(");
-
-    var server = Server.init(gpa, child);
-    defer server.deinit();
-
-    try server.serveStringMessage(.rawString, "Hello from server!");
-    try server.serveExit();
-
-    const timer = try std.time.Instant.now();
-    while ((try std.time.Instant.now()).since(timer) < std.time.ns_per_s * 30) {
-        switch (try server.receiveMessage(gpa)) {
-            .Message => |thing| printf("server received: {any}", .{thing}),
-            .TimedOut => continue,
-            .StreamClosed => break,
-        }
-    }
-
-    _ = try child.kill();
-
-    for (server.process_streamer.output_metadata.items) |item| {
-        const array_list = switch (item.tag) {
-            .stdout => server.process_streamer.stdout_content,
-            .stderr => server.process_streamer.stderr_content,
-        };
-
-        const full_slice = array_list.items[item.start_idx..(item.start_idx + item.len)];
-
-        var last_idx: usize = 0;
-        for (full_slice, 0..) |char, idx| {
-            if (char != '\n') continue;
-            const slice = full_slice[last_idx..idx];
-
-            if (slice.len == 0) continue;
-            std.debug.print("{s} | {s}\n", .{ @tagName(item.tag), slice });
-
-            last_idx = idx + 1;
-        }
-        const slice = full_slice[last_idx..];
-
-        if (slice.len == 0) continue;
-        std.debug.print("{s} | {s}\n", .{ @tagName(item.tag), slice });
-    }
+fn termiosReset(fd: std.posix.fd_t, term: std.posix.termios) !void {
+    try std.posix.tcsetattr(
+        fd,
+        std.posix.TCSA.FLUSH,
+        term,
+    );
 }
