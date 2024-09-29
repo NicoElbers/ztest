e515df33b20147a69c2faf8fd9c1c2bde9fe818f
Nico Elbers <nico.b.elbers@gmail.com>
2024-09-29 13:24:13 +0200

    Refactor: Remove ServerStreamer, ClientStreamer and streamerUtils
    ServerStreamer and Clientstreamer did basically the exact same thing,
    but were completely seperate types. Now, Client and Server streamer are
    both part of the Poller generic type. This is overall less code, and it
    means I can remove the jankey ass streamerUtils, which I never liked.
    
    ServerStreamer: Move to more generic Poller type
    
    ClientStreamer: Move to generic Poller type
    
    streamerUtils: Inline that bitch

--- src/runnerIPC/root.zig
+++ src/runnerIPC/root.zig
@@ -1,17 +1,16 @@
 pub const Server = @import("Server.zig");
-pub const ServerStreamer = @import("ServerStreamer.zig");
-
 pub const Client = @import("Client.zig");
-pub const ClientStreamer = @import("ClientStreamer.zig");
+
+pub const Poller = @import("poller.zig").Poller;
 
 pub const Message = @import("Message.zig");
 
 test {
     _ = @import("Server.zig");
-    _ = @import("ServerStreamer.zig");
-
     _ = @import("Client.zig");
-    _ = @import("ClientStreamer.zig");
+
+    _ = Poller(.client);
+    _ = Poller(.server);
 
     _ = @import("Message.zig");
 }
